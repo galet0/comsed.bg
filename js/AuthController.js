27df7dcaf2074bec4731fb9c2b85bb22e9b3aec7
veselaIl <veselailchevska@yahoo.com>
2018-04-05 07:38:55 +0300

    register.html, authentication.html

---
+++ js/AuthController.js
@@ -0,0 +1,49 @@
+var pageElement = document.querySelector('.container'),
+    register = pageElement.querySelector('.register'),
+    registerBtn = register.querySelector('.register-btn');
+
+
+    function initRegister(){
+
+        var form = document.querySelector('.register-form'),
+            firstName = form.querySelector('input [name="firstName"]'),
+            lastName = form.querySelector('input [name="lastName"]'),
+            email = form.querySelector('input [name="email"]'),
+            password = form.querySelector('input[name="password"]');
+            passwordConfirm = form.querySelector('input [name="passwordConfirm"]'),
+            error = form.querySelector('.error');
+
+            form.addEventListener('submit', function(event){
+                console.log(event);
+                event.preventDefault();
+                if(password.value === passwordConfirm.value){
+                    if(password.value.length > 5){
+                        if(user = UserModel.register(email.value,password.value)){
+                            console.log('register confirm')
+                        }else{
+                            username.value = '';
+                            password.value = '';
+                            passwordAgain.value = '';
+                            error.textContent = 'That user already exists!';
+                            error.style.display = 'block';
+                        }
+                    }else{
+                        password.value = '';
+                        passwordAgain.value = '';
+                        error.textContent = 'Password is too short!';
+                        error.style.display = 'block';
+                    }
+                }else{
+                    password.value = '';
+                    passwordAgain.value = '';
+                    error.textContent = 'Passwords do not match!';
+                    error.style.display = 'block';
+                }
+            });
+    }
+
+    registerBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log(event);
+        initRegister();
+    });
---
+++ js/UserModel.js
@@ -0,0 +1,39 @@
+var UserModel = (function(){
+    function User(firstName, lastName, email,password){
+        this.userID = User.prototype.nextID++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+    User.prototype.nextID = 1;
+
+    var UserStorage = {
+        register: function(firstName, lastName,email, password) {
+            var user = new User(firstName,lastName, email, password);
+            users.push(user);
+            return user;
+        },
+        login: function(email, password) {
+            return users.find(function(user) {
+                return user.email === email && user.password === password;
+            });
+        },
+        hasUser: function(email) {
+            return !!users.find(function(user) {
+                return user.email === email;
+            });
+        }
+    };
+    var users = [];
+
+    users.push(new User('ivan', '123456'));
+
+    return {
+        register: function(email, password) {
+            if (!UserStorage.hasUser(email)) {
+                return UserStorage.register(firstName, lastName, email,password);
+            }
+        },
+        login: UserStorage.login
+    };
+})();
