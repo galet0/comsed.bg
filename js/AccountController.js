a126b539ae61532d06aca6a8041c1d08c076a7f6
veselaIl <veselailchevska@yahoo.com>
2018-04-12 16:06:43 +0300

    index pagе

--- js/AccountController.js
+++ js/AccountController.js
@@ -3,12 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var loggedUser = sessionStorage.getItem('logged');
     var users = JSON.parse(window.localStorage.getItem('users'));
     var user = users.find(x => x.email === loggedUser);
-
-    var indexProfile = getTemplate('indexProfile');
-    var template = Handlebars.compile(indexProfile);
-    var temp = template(user);
-    document.querySelector('.reg-cart').innerHTML = temp;
-
-
+    if(user){
+        var indexProfile = getTemplate('indexProfile');
+        var template = Handlebars.compile(indexProfile);
+        var temp = template(user);
+        document.querySelector('.headderAuth').innerHTML = temp;
+        document.querySelector('.accountBtn').addEventListener('click',function(event){
+            event.preventDefault();
+            window.location = 'account.html';
+        })
+    }
+    
+    
 
 });
